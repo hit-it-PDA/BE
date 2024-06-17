3706647409fb14088f25bbccabda6dd5382ef2b7
seohee <53520867+seohee99@users.noreply.github.com>
2024-06-17 13:07:24 +0900

    Revert "chore: investment-service 모듈 분리"

--- investment-service/src/main/java/com/pda/investment_service/InvestmentServiceApplication.java
+++ investment-service/src/main/java/com/pda/investment_service/InvestmentServiceApplication.java
@@ -1,13 +1,13 @@
-package com.pda.user_service;
+package com.pda.investment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class InvestmentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(InvestmentServiceApplication.class, args);
 	}
 
 }
--- mydata-service/src/main/java/com/pda/mydata_service/MydataServiceApplication.java
+++ mydata-service/src/main/java/com/pda/mydata_service/MydataServiceApplication.java
@@ -3,7 +3,7 @@ package com.pda.mydata_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.pda")
+@SpringBootApplication
 public class MydataServiceApplication {
 
 	public static void main(String[] args) {
